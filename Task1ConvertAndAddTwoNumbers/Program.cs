e218cda50bbdb7cbe8b0dfb429612e659fed6924
Desislava.Kostadinova <desislava.kostadinova@blankfactor.com>
2024-11-26 11:13:31 +0200

    Homework1.sln

---
+++ Task1ConvertAndAddTwoNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, user! Please, enter a number: ");
+string userInput = Console.ReadLine();
+Console.WriteLine("Thanks! Now another one, please: ");
+string userInput2 = Console.ReadLine();
+int userInput3 = int.Parse(userInput);
+int userInput4 = int.Parse(userInput2);
+Console.WriteLine("The sum of your two numbers is " + (userInput3+userInput4));
