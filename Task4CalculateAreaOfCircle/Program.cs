6fda0fc3ee90338cbe115255943dbf11914351d9
Desislava.Kostadinova <desislava.kostadinova@blankfactor.com>
2024-11-27 14:26:17 +0200

    Updates to Task1CinemaHall

--- Task4CalculateAreaOfCircle/Program.cs
+++ Task4CalculateAreaOfCircle/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Radius: ");
+using System;
+
+Console.WriteLine("Radius: ");
 string userInput = Console.ReadLine();
 double radius = double.Parse(userInput);
 double area = Math.PI * radius * radius;
