b696dee7fcc9d9c7e86fbfe0f33f2f75c832a729
Desislava.Kostadinova <desislava.kostadinova@blankfactor.com>
2024-11-27 11:15:45 +0200

    Updates to task2FormatFullName

--- Task2FormatAFullName/Program.cs
+++ Task2FormatAFullName/Program.cs
@@ -4,4 +4,4 @@ Console.WriteLine("Enter your first name:");
 string firstName = Console.ReadLine();
 Console.WriteLine("Enter your last name:");
 string lastName = Console.ReadLine();
-Console.WriteLine("Hello, " + lastName + ", " + firstName + ".");
+Console.WriteLine($"Hello, {lastName}, {firstName}!");
