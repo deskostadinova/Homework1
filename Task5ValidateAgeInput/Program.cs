5cd35e082761e622012fc69f8906171b915418ae
Desislava.Kostadinova <desislava.kostadinova@blankfactor.com>
2024-11-27 11:38:44 +0200

    updates to task 4 ValidateAgeInput

--- Task5ValidateAgeInput/Program.cs
+++ Task5ValidateAgeInput/Program.cs
@@ -1,8 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int age;
+string userInput;
+bool isAgeParsed = false;
 
-Console.WriteLine("Please, enter your age: ");
-while (!int.TryParse(Console.ReadLine(), out age))
+Console.WriteLine("Enter your age: ");
+
+while (!isAgeParsed)
 {
+    userInput = Console.ReadLine();
+    isAgeParsed = int.TryParse(userInput, out age);
+    if(isAgeParsed)
+    {
+        Console.WriteLine($"Age read as {age}");
+        break;
+    }
     Console.WriteLine("Please, enter a valid age: ");
 }
